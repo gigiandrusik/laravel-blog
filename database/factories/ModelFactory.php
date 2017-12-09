55794d252762cea728ae28e1ea0ab3f46a1df0eb
Andrusik <gigiandrusik@i.ua>
2017-12-09 17:19:18 +0200

    init project

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+/**@var $factory Illuminate\Database\Eloquent\Factory */
